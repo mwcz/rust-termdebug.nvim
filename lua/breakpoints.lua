7ac667a1c58b7666c35c8c273064f111fdaddd5f
mwcz <mwc@clayto.com>
2025-07-21 23:55:28 -0400

    much better way to delete breakpoint under cursor

--- lua/breakpoints.lua
+++ lua/breakpoints.lua
@@ -10,17 +10,7 @@ end
 
 -- clear breakpoints on the current line
 M.clear_curline = function()
-    local file_path = vim.fn.expand("%:p")
-    if file_path == "" then
-        vim.notify("Current filepath (%:p) was empty string", vim.log.levels.ERROR)
-        return
-    end
-
-    local line_nr = vim.fn.line(".")
-
-    local command = "clear " .. file_path .. ":" .. line_nr
-
-    vim.fn.TermDebugSendCommand(command)
+    vim.cmd("Clear")
 end
 
 return M
